34596f9598937ab202c688b735a461d9b3b53c76
RedSnoww12 <70609663+RedSnoww12@users.noreply.github.com>
2022-03-18 18:32:17 +0100

    first commit

---
+++ Modele/CompteEpargne.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banque.Modele
+{
+    class CompteEpargne : Compte
+    {
+
+        static double taux;
+
+        public CompteEpargne(int n, Client c, double unTaux) : base(n, c)
+        {
+            CompteEpargne.taux = unTaux;
+
+        }
+
+
+        public override void debiter(double mont)
+        {
+            if (solde - mont < 0)
+            {
+                throw (new Exception("Débit interdit"));
+            }
+            else
+            {
+                solde = solde - mont;
+            }
+        }
+
+        public override string Description
+        {
+            get => base.Description + " taux : " + CompteEpargne.taux;
+        }
+    }
+}
